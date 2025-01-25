fc901f9ed4d5cb94ff52282b41de0980cac7d695
Nikit2002 <ndubinin2002@gmail.com>
2025-01-25 12:10:44 +0200

    Update autoScroller.js

--- src/js/autoScroller.js
+++ src/js/autoScroller.js
@@ -5,10 +5,5 @@
                     scroller.setAttribute("data-animated", true);
                     const scrollerInner = scroller.querySelector(".scroller__inner");
                     const scrollerContent = Array.from(scrollerInner.children);
-                    scrollerContent.forEach((item) => {
-                      const duplicatedItem = item.cloneNode(true);
-                      duplicatedItem.setAttribute("aria-hidden", true);
-                      scrollerInner.appendChild(duplicatedItem);
-                    });
                   });
                 }
